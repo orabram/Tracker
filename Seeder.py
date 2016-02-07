94d03024f6abad5dd81f14c8ee1fb6751ce1c96e
orabram <or.abramovich1@gmail.com>
2016-02-07 18:33:55 +0200

    Added Seeder
    This class will be used to work with the seeders. Each one will be
    represented with this class.

---
+++ Seeder.py
@@ -0,0 +1,38 @@
+__author__ = 'Or'
+import socket
+
+BUFFER = 4098
+
+class Seeder():
+    def __int__(self, ip, port, socket):
+        self.ip = ip
+        self.port = port
+        self.socket = socket
+        self.files = []
+        self.profile = 0
+
+    def mark_suspicious_files(self, filename):
+        self.socket.send("mark#" + filename)
+
+    def remove_file(self, filename):
+        self.socket.send("remove#" + filename)
+        self.files.remove(filename)
+
+    def add_new_file(self, filename, chunks):
+        self.socket.send("add#" + filename)
+        self.socket.send(chunks)
+        self.files.add(filename)
+
+    def modify_files_list(self, new_list):
+        self.socket.send("list#" + new_list)
+        self.files = new_list
+
+    def get_computer_stats(self):
+        self.socket.send("profile")
+        return self.socket.recv(BUFFER)
+
+    def set_profile(self, profile):
+        self.profile = profile
+
+    def get_profile(self):
+        return self.profile
