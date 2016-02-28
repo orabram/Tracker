a8e006791e5e2ce98ba6bd66aa6842f44f4ac8bb
orabram <34v7@34v7-06.tichon.guttman>
2016-02-28 09:57:19 +0200

    Added struct support
    My code now works with the struct module. There currently aren't any
    problems with the encryption I've made with him, but I should expect
    them.

--- ClientManager.py
+++ ClientManager.py
@@ -3,18 +3,20 @@ import struct
 import socket
 import time
 import random
+from SeedersManager import *
 
 BUFFER = 4096
 MIN_CONNECTION_ID = 10000000
 MAX_CONNECTION_ID = 99999999
 
 class ClientManager():
-    def __init__(self, ip, port):
+    def __init__(self, ip, port, seeder_manager):
         self.interval = 120
         self.files = {}
         self.downloaders = {}
         self.ip = ip
         self.port = port
+        self.seeder_manager = seeder_manager()
 
     def wait_for_connections(self):
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -25,18 +27,27 @@ class ClientManager():
 
 
     def ParseRequest(self, packet, client_address, socket):
-        action = packet[8:12]
+        packet = struct.unpack(">qii")[0]
+        action = int(packet[8:12])
         if action == 0:
             connection_id = random.randrange(MIN_CONNECTION_ID, MAX_CONNECTION_ID)
-            socket.sendto(action + packet[0:8] + connection_id, client_address)
+            transaction_id = packet[12:16]
+            packet = action + transaction_id + connection_id
+            packet = struct.pack(packet, ">iiq")
+            socket.sendto(packet, client_address)
             self.downloaders[str(client_address)] = (str(time.time()) + "#" + str(connection_id) + "#None")
         elif action == 1:
+            packet = struct.unpack(">qiissqqqiiiih")[0]
             connection_id = packet[:8]
-            currect_time = time.time()
+            current_time = time.time()
             connection_info = self.downloaders[str(client_address)].split("#")
-            if currect_time - int(connection_info[0]) < self.interval:
+            if current_time - int(connection_info[0]) < self.interval:
                 if connection_info[1] == connection_id:
                     transaction_id = packet[12:16]
+                    leechers = 0
+                    seeders = len(self.downloaders)
+                    seeders_dict = {}
+
 
 
 
