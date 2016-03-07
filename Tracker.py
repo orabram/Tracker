b4254a610e14bacf1043776047e75e3bd7d93506
orabram <or.abramovich1@gmail.com>
2016-03-08 00:53:53 +0200

    Minor Changes

--- Tracker.py
+++ Tracker.py
@@ -1,5 +1,5 @@
 __author__ = 'Or'
-from Seeder import *
+from ClientManager import *
 from SeedersManager import *
 from GUIManager import *
 import socket
@@ -8,10 +8,10 @@ from multiprocessing import Process
 
 SELF_IP = "0.0.0.0"
 GUI_PORT = 6666
+TRACKER_PORT = 3456
 
+seeders_manager = seeder_communication_manager()
+clients_manager = ClientManager(SELF_IP, TRACKER_PORT, seeders_manager)
 
-
-
-seedersmanager = seeder_communication_manager()
 
 
