7948bd59c09c185a868dc871d6d37eca57202d93
orabram <or.abramovich1@gmail.com>
2016-03-08 13:26:11 +0200

    Updated Tracker
    I've added some of the necessary methods to the program.

--- Tracker.py
+++ Tracker.py
@@ -10,8 +10,29 @@ SELF_IP = "0.0.0.0"
 GUI_PORT = 6666
 TRACKER_PORT = 3456
 
+def start_processes():
+    p = Process(target=clients_manager.wait_for_connections)
+    p.start()
+    p = Process(target=connect_to_gui, args=[gui_manager])
+    p.start()
+    p = Process(target=get_new_commands)
+
+
+def connect_to_gui(gui_manager):
+    gui_manager.establish_connection()
+    time1 = time.time()
+    while True:
+        if time.time() - time1 > 120:
+            time1 = time.time()
+            gui_manager.send_computers_list()
+
+
+def get_new_commands():
+
+
 seeders_manager = seeder_communication_manager()
 clients_manager = ClientManager(SELF_IP, TRACKER_PORT, seeders_manager)
+gui_manager = gui_manager()
 
 
 
