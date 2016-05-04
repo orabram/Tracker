cb39354614e9baffef1b776833b98aa1a4768597
orabram <or.abramovich1@gmail.com>
2016-05-04 08:46:30 +0300

    More Bug Fixes

---
+++ EncryptionKeyMaker.py
@@ -0,0 +1,49 @@
+#region -----------------Info-----------------
+#Name: Encryption Key Maker
+#Version: 1.0
+#By: Or Abramovich
+#endregion -----------------Info-----------------
+
+#region -----------------Imports-----------------
+from Settings import *
+from Printer import *
+from Singleton import *
+from Encryptor import *
+from socket import *
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
+from Crypto.Cipher import AES
+#endregion -----------------Imports-----------------
+
+#region -----------------Class-----------------
+
+
+class EncryptionKeyMaker(object):
+    # This class is used for creating symmetric encryption keys for communicating with phones.
+    __metaclass__ = Singleton
+    printer = Printer()
+    settings = Settings()
+    __pure_encryption_key = None # The asymmetric encryption key that is used for the process of creating the symmetric key.
+    __encryption_key = None
+
+    def __init__(self):
+        self.printer.printMessage(self.__class__.__name__, 'Generating RSA key.')
+        self.__pure_encryption_key = RSA.generate(int(self.settings.getSetting('RSA_bits')))
+        self.__encryption_key = PKCS1_v1_5.new(self.__pure_encryption_key)
+        self.printer.printMessage(self.__class__.__name__, 'RSA key generated.')
+
+    def createEncryptor(self, phone):
+        # Communicates with the given socket and creates a symmetric key that is used by both sides.
+        errors_param = None
+        public_key = self.__pure_encryption_key.publickey().exportKey(format='DER')
+        print 'public key: ' + public_key
+        phone.raw_send(public_key)
+        encrypted_key = phone.raw_recv()
+        print 'key: ' + encrypted_key
+        print 'key len: ' + str(len(encrypted_key))
+        phone_key = self.__encryption_key.decrypt(encrypted_key, errors_param)
+        print 'phone key: ' + phone_key
+        return Encryptor(AES.new(phone_key))
+
+
+#endregion -----------------Class-----------------
---
+++ Encryptor.py
@@ -0,0 +1,58 @@
+#region -----------------Info-----------------
+#Name: Encryptor
+#Version: 1.0
+#By: Or Abramovich
+#endregion -----------------Info-----------------
+
+#region -----------------Imports-----------------
+from Settings import *
+from Printer import *
+#endregion -----------------Imports-----------------
+
+#region -----------------Class-----------------
+
+
+class Encryptor(object):
+    # An object that simplifies the use of symmetric keys.
+    __encryption_key = None # The symmetric key that the object uses.
+
+    def __init__(self, encryption_key):
+        self.__encryption_key = encryption_key
+
+    def encrypt(self, message):
+        # Adds padding to the message and encrypts it.
+        padded_message = addPadding(message)
+        encrypted_message = self.__encryption_key.encrypt(padded_message)
+        return encrypted_message
+
+    def decrypt(self, message):
+        # Decrypts the message and removes the padding.
+        try:
+            print 'after base64: ' + message
+            padded_message = self.__encryption_key.decrypt(message)
+            print 'after decryption: ' + padded_message
+            unpadded_message = removePadding(padded_message)
+            print 'after unpadding: ' + unpadded_message
+        except ValueError:
+            unpadded_message = message
+        except TypeError:
+            unpadded_message = message
+        except IndexError:
+            unpadded_message = message
+        return unpadded_message
+
+def addPadding(message):
+        # Adds padding to the message
+        pad_len = 16 - len(message) % 16
+        padded_message = message.ljust(len(message) + pad_len, chr(pad_len))
+        return padded_message
+
+def removePadding(message):
+    # Removes padding from the message.
+    pad_len = ord(message[-1])
+    if pad_len >= 16:
+        return message
+    real_message = message[:-pad_len]
+    return real_message
+
+#endregion -----------------Class-----------------
